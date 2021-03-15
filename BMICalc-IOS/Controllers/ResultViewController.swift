7de846587815b6ce37b332065b9ee34a0f19cf8a
danielliu1 <dliu1.dev@gmail.com>
2021-03-15 03:52:03 -0700

    Second view controller

--- BMICalc-IOS/Controllers/ResultViewController.swift
+++ BMICalc-IOS/Controllers/ResultViewController.swift
@@ -12,17 +12,16 @@ class ResultViewController: UIViewController {
     var bmiValue: String?
     
     @IBOutlet weak var bmiLabel: UILabel!
-    
     @IBOutlet weak var adviceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
- 
+        bmiLabel.text = bmiValue
     }
     
 
     @IBAction func recalculatePressed(_ sender: Any) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
     
 }
